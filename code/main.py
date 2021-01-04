70c19f6fd78d4e16ce3dc3eb1fede5cf038d6b6c
3michelin <remingtonskim@gmail.com>
2021-01-04 14:25:48 -0500

    basic tracking and print to console

--- code/main.py
+++ code/main.py
@@ -4,6 +4,10 @@ import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
+# just face detection built into Open CV, must have it installed in C: drive
+upper_body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades+ 'haarcascade_frontalface_alt.xml')
+# upper_body_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+
 
 while(True):
     # Capture frame-by-frame
@@ -12,7 +16,15 @@ while(True):
     # Our operations on the frame come here
     # apply filters here probably
     # changing color hue can be done here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+
+    # the detection
+    upper_body = upper_body_cascade.detectMultiScale(gray, 1.3, 5)
+
+    # prints data to console for now
+    print(upper_body)
+
 
     # Display the resulting frame
     cv2.imshow('frame',frame)

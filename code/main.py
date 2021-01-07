529fc6fa52f848acb11af68435d9c03914185b0b
3michelin <remingtonskim@gmail.com>
2021-01-07 13:50:08 -0500

    update virtual cam

--- code/main.py
+++ code/main.py
@@ -2,12 +2,15 @@
 # https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_gui/py_video_display/py_video_display.html
 import numpy as np
 import cv2
+from pyvirtualcam import *
+
 f = open('./haarcascade_frontalface_alt.xml', 'r')
 cap = cv2.VideoCapture(0)
 # just face detection built into Open CV, must have it installed in C: drive
 upper_body_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 # upper_body_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 grey = 0
+
 
 while(True):
     # Capture frame-by-frame
@@ -23,7 +26,6 @@ while(True):
     # changing color hue can be done here
     gray = cv2.cvtColor(frame, color)
 
-    
     # the detection
     upper_body = upper_body_cascade.detectMultiScale(gray, 1.3, 5)
 
@@ -35,12 +37,14 @@ while(True):
     font = cv2.FONT_HERSHEY_SIMPLEX
     cv2.putText(gray,'HiRemi',(10,300), font, 4,(255,255,255),2,cv2.LINE_AA)
     cv2.imshow('frame',gray)
-     
+    
     k = cv2.waitKey(1) & 0xFF
     if k == ord('m'):
         grey += 1
     elif k == 27:
         break
+
+        
 
 # When everything done, release the capture
 cap.release()
